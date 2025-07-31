a60c42dd9b655fa8dce1ddb0522aeb460efb288f
fra <mastercaster36@outlook.it>
2025-07-31 13:04:02 +0200

    styles: styling meter bar (wip)

--- src/main.js
+++ src/main.js
@@ -1,1 +1,9 @@
 import './style.scss'
+
+const meterWrapper = document.getElementById('meter-wrapper')
+
+document.addEventListener('DOMContentLoaded', function () {
+   const meter = document.querySelector('meter')
+   const leftoverPercentage = ((meter.max - meter.value) / meter.max) * 100; 
+   meterWrapper.style.setProperty('--white-circle-position', `${leftoverPercentage}%`)
+})
