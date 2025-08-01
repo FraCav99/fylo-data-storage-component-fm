157a4f14ee21f42a1c66f6c59b60c77554bd2586
fra <mastercaster36@outlook.it>
2025-08-01 10:53:23 +0200

    all: changed markup, logic and styles to dynamically update the bar

--- src/main.js
+++ src/main.js
@@ -1,9 +1,19 @@
 import './style.scss'
 
-const meterWrapper = document.getElementById('meter-wrapper')
+const meter = document.querySelector("meter") 
+const meterSlider = document.querySelector(".meter-slider") 
 
-document.addEventListener('DOMContentLoaded', function () {
-   const meter = document.querySelector('meter')
-   const leftoverPercentage = ((meter.max - meter.value) / meter.max) * 100; 
-   meterWrapper.style.setProperty('--white-circle-position', `${leftoverPercentage}%`)
-})
+function updateMeterSliderWidth() {
+   const percentage = (meter.value / meter.max) * 100
+    // Calculate the minimum percentage width based on the parent element's width
+    const wrapperWidth = meterSlider.parentElement.clientWidth;
+    const minWidthPercentage = (20 / wrapperWidth) * 100;
+
+    // Ensure the percentage is at least the minimum width percentage
+    const finalPercentage = Math.max(percentage, minWidthPercentage);
+
+    // Set the --slider-width CSS variable
+    meterSlider.style.setProperty('--slider-width', `${finalPercentage}%`);
+}
+
+updateMeterSliderWidth()
